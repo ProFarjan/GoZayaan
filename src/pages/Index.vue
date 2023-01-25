a9e9340651f5a16f505dca7f6045da11b28ff1b0
Farjan <farjanhasannias@gmail.com>
2023-01-25 20:12:41 +0600

    design changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,6 +27,55 @@
               </b-form-radio-group>
             </div>
 
+            <b-card-footer id="searchbar">
+              <div class="flight-search bar">
+                <div class="box location from">
+                  <span class="label">From</span>
+                  <div class="value">Dhaka</div>
+                  <span class="sub-value">
+                    DAC, Hazrat Shahjalal International Airport
+                  </span>
+                </div>
+                <div class="box location to has-swapper">
+                  <span class="swapper"></span><span class="label">To</span>
+                  <div class="value">
+                    Cox's Bazar
+                  </div>
+                  <span class="sub-value">
+                    CXB, Cox's Bazar Airport
+                  </span>
+                </div>
+                <div class="box date depart">
+                  <span class="label">Journey Date</span>
+                  <div class="value">
+                    30 <span>Jan'23</span>
+                  </div>
+                  <span class="sub-value">
+                    Monday
+                  </span>
+                </div>
+                <div class="box date return">
+                  <span class="label">Return Date</span>
+                  <div class="value">
+                    31 <span>Jan'23</span>
+                  </div>
+                  <span class="sub-value">
+                    Tuesday
+                  </span>
+                  <span class="remove-return-date"></span>
+                </div>
+                <div class="box traveler">
+                  <span class="label">Traveler, Class</span>
+                  <div class="value">1
+                    Traveler
+                  </div>
+                  <span class="sub-value">
+                    Economy
+                  </span>
+                </div>
+              </div>
+            </b-card-footer>
+
           </b-card>
 
         </div>
@@ -128,5 +177,71 @@ export default {
   #btn_flight_radio span {
     color: #1c3c6b;
     opacity: 1;
+  }
+
+
+  #searchbar .bar {
+    display: flex;
+  }
+  #searchbar div.location {
+    flex-basis: calc(25% - 10px);
+    max-width: calc(25% - 10px);
+    margin-right: 10px !important;
+  }
+  #searchbar .bar div.box {
+    padding: 8px 15px;
+    border-radius: 10px;
+    border: 1px solid #dbdde0;
+    cursor: pointer;
+    position: relative;
+  }
+  #searchbar .bar div.box .label {
+    font-size: .8571428571rem;
+    line-height: 14px;
+    font-weight: 400;
+    display: block;
+  }
+  #searchbar .box span.label {
+    text-transform: uppercase;
+    color: #1c3c6b;
+    margin-bottom: 4px;
+  }
+  #searchbar .bar div.box .value {
+    font-size: 1.2857142857rem;
+    line-height: 24px;
+    font-weight: 700;
+    color: #1c3c6b;
+  }
+  #searchbar .bar div.box .value+.sub-value {
+    margin-top: 0;
+    font-size: 12px;
+  }
+  #searchbar .bar div.box .sub-value {
+    line-height: 14px;
+    font-weight: 400;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  #searchbar .box span.sub-value {
+    color: #5d6974;
+  }
+  #searchbar .bar div.box.has-swapper {
+    padding-left: 25px;
+  }
+  #searchbar .bar div.box .swapper {
+    position: absolute;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background-image: url(http://localhost:8080/img/swap.svg);
+    background-size: 40px;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    top: 50%;
+    left: -5px;
+    -webkit-transform: translate(-50%,-50%);
+    transform: translate(-50%,-50%);
   }
 </style>
